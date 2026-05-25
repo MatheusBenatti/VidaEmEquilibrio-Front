3c208cf53c83788536618d5de6b570dd833e3061
Matheus <theusbenatti@gmail.com>
2026-05-24 22:06:50 -0300

    chore: tentando vercel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import HomePsicologo from './pages/HomePsicologo';
